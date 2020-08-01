ab479ad5d94537a62b925f60342dee55bc0d5496
Mussy <mussymuss@gmail.com>
2020-08-01 15:32:54 -0400

    Final Check

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Chicken', TRUE);
